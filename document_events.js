ac29e004bf8ed56e7b6cc242ac3a580d73cf2080
mano1003 <mano1003@hotmail.com>
2024-10-27 19:57:41 -0700

    completed

--- document_events.js
+++ document_events.js
@@ -24,7 +24,35 @@ function displayColorList(document) {
 document.dispatchEvent(new window.KeyboardEvent('keyup', {keyCode: 32}));
 function removeColorList(document) {
     // Your code goes here.
-    const colorList = document.getElementById("color-list");
-    colorList.remove();
-    document.write();
+    document.addEventListener("keyup", () => {
+        /* Their Linked In Answer
+        const container = document.getElementById("container");
+        const colorList = document.getElementById("color-list");
+        container.removeChild(colorList);
+        */
+       // My Answer
+       const colorList = document.getElementById("color-list");
+       colorList.remove();
+    });
+}
+// Problem - 3
+/*
+ On drop down selection - onchange display the selected value
+*/
+myFavoriteShowSelect.dispatchEvent(new window.Event('change'));
+function displayMessageOnChange(document) {
+    // Your code goes here.
+    const divresult = document.getElementById("result");
+    const selectOptions = document.getElementById("favorite-tv-show");
+    /* Their answer
+    selectOptions.addEventListener("change", (event) => {
+        //const selectedText = selectOptions.options[selectOptions.selectedIndex];
+        divresult.innerHTML = "Your selection changed to: " + event.target.value;
+    });
+    */
+   // My Answer
+    selectOptions.addEventListener("change", () => {
+        const selectedText = selectOptions.options[selectOptions.selectedIndex];
+        divresult.innerHTML = "Your selection changed to: " + selectedText.text;
+    });
 }
